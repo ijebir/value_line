56c651d12726df7bfff09d0a8a98387e072f1b0c
Idriss Jebir <idriss.jebir@gmail.com>
2025-08-27 19:50:59 +0100

    init

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-print("hello")
+import yfinance as yf
+
+apple = yf.Ticker("aapl")
+
+# pandas dataFrame containing dividends and splits
+corpAction = apple.actions
+
+# pandas dataFrame containing historical accounting data
+financials = apple.financials
+financialsTimestamps = financials.columns.values.tolist()
+
+print(financialsTimestamps)
